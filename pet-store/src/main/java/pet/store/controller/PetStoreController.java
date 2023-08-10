164a25545927e64a92a8ba99e0d9fc83ff4bbcb5
RKVanHorn <acinonyx79@hotmail.com>
2023-08-09 20:54:21 -0700

    added an update employee method and mapping

--- pet-store/src/main/java/pet/store/controller/PetStoreController.java
+++ pet-store/src/main/java/pet/store/controller/PetStoreController.java
@@ -42,38 +42,46 @@ public class PetStoreController {
 		log.info("Updating pet store {}", petStoreData);
 		return petStoreService.savePetStore(petStoreData);
 	}
-	
+
 	@PostMapping("/{petStoreId}/employee")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public PetStoreEmployee addEmployee(@PathVariable Long petStoreId, @RequestBody PetStoreEmployee petStoreEmployee) {
 		log.info("Adding employee{} to pet store with ID={}", petStoreEmployee, petStoreId);
-		
+
 		return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
 	}
-	
+
 	@PostMapping("/{petStoreId}/customer")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public PetStoreCustomer addCustomer(@PathVariable Long petStoreId, @RequestBody PetStoreCustomer petStoreCustomer) {
 		log.info("Adding customer {} to petstore with ID={}", petStoreCustomer, petStoreId);
 		return petStoreService.saveCustomer(petStoreId, petStoreCustomer);
 	}
-	
-	@GetMapping("")
-	public List<PetStoreData> listAllPetStores(){
+
+	@GetMapping
+	public List<PetStoreData> listAllPetStores() {
 		log.info("Listing all pet stores");
 		return petStoreService.retrieveAllPetStores();
 	}
-	
+
 	@GetMapping("/{petStoreId}")
 	public PetStoreData returnPetStoreById(@PathVariable Long petStoreId) {
 		log.info("Retrieving pet store with ID={}", petStoreId);
 		return petStoreService.returnPetStoreById(petStoreId);
 	}
-	
+
 	@DeleteMapping("/{petStoreId}")
 	public Map<String, String> deletePetStoreById(@PathVariable Long petStoreId) {
 		log.info("Removing pet store with ID={}", petStoreId);
 		petStoreService.deletePetStoreById(petStoreId);
 		return Map.of("message", "Successfully deleted pet store with ID=" + petStoreId);
+	}
+
+	@PutMapping("/{petStoreId}/employee/{employeeId}")
+	public PetStoreEmployee updateEmployee(@PathVariable Long petStoreId, @PathVariable Long employeeId,
+			@RequestBody PetStoreEmployee petStoreEmployee) {
+		log.info("Updating employee with ID={} at pet store with ID={}", employeeId, petStoreId);
+		petStoreEmployee.setEmployeeId(employeeId);
+		return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
 	}
 }
